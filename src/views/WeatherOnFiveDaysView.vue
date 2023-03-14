0feb450e7a5e6ccbb5d8e43420821b0c2c61cb10
Juris-Berkulis <krztfjrbhm1@jmail.com>
2023-03-14 16:26:44 +0300

    1. Change the styles assignment method for the "WeatherOnFiveDaysView" component; 2. Make minor improvements in the "WeatherOnFiveDaysView" component.

--- src/views/WeatherOnFiveDaysView.vue
+++ src/views/WeatherOnFiveDaysView.vue
@@ -6,8 +6,6 @@ export default {
         ...mapState({
             weatherOnFiveDays: (state) => state.weatherModule.weatherOnFiveDays,
         }),
-    },
-    methods: {
         getMaxTempForFiveDays() {
             let minTemp = this.weatherOnFiveDays.list[0].main.temp;
             let maxTemp = this.weatherOnFiveDays.list[0].main.temp;
@@ -22,19 +20,37 @@ export default {
 
             return {minTemp, maxTemp}
         },
-    }
+    },
 }
 </script>
 
 <template>
-    <div style="display: flex; overflow-x: auto;" v-if="weatherOnFiveDays">
-        <div style="width: 50px; min-width: 50px; display: flex; flex-direction: column; justify-content: flex-end; align-items: center; margin: 5px;" v-for="dayWeather in weatherOnFiveDays.list">
-            <div :style="{width: '100%', backgroundColor: 'darkblue', height: 100 * (dayWeather.main.temp - getMaxTempForFiveDays().minTemp) / (getMaxTempForFiveDays().maxTemp - getMaxTempForFiveDays().minTemp) + 5 + 'px'}"></div>
+    <div class="weatherOnFiveDaysTable" v-if="weatherOnFiveDays">
+        <div class="eachDay" v-for="dayWeather in weatherOnFiveDays.list">
+            <div class="eachDayColumn" :style="{height: 5 + 95 * (dayWeather.main.temp - getMaxTempForFiveDays.minTemp) / (getMaxTempForFiveDays.maxTemp - getMaxTempForFiveDays.minTemp) + 'px'}"></div>
             <div>{{ dayWeather.main.temp }}°</div>
         </div>
     </div>
 </template>
 
 <style>
+.weatherOnFiveDaysTable {
+    display: flex;
+    overflow-x: auto;
+}
 
+.eachDay {
+    width: 50px;
+    min-width: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
+    margin: 5px;
+}
+
+.eachDayColumn {
+    width: 100%;
+    background-color: darkblue;
+}
 </style>
