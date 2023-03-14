066fefaa4374b213eb56554b2fbce2a26de9e0a2
Juris-Berkulis <krztfjrbhm1@jmail.com>
2023-03-14 17:07:34 +0300

    1. Add indicators for the "WeatherOnFiveDaysView" component; 2. Add styles for the "WeatherOnFiveDaysView" component.

--- src/views/WeatherOnFiveDaysView.vue
+++ src/views/WeatherOnFiveDaysView.vue
@@ -21,26 +21,48 @@ export default {
             return {minTemp, maxTemp}
         },
     },
+    methods: {
+        getIcon(weatherObj) {
+            const imgSrc = `https://openweathermap.org/img/w/${weatherObj.icon}.png`;
+            const imgAlt = weatherObj.main;
+            return {imgSrc, imgAlt}
+        },
+    }
 }
 </script>
 
 <template>
-    <div class="weatherOnFiveDaysTable" v-if="weatherOnFiveDays">
-        <div class="eachDay" v-for="dayWeather in weatherOnFiveDays.list">
-            <div class="eachDayColumn" :style="{height: 5 + 95 * (dayWeather.main.temp - getMaxTempForFiveDays.minTemp) / (getMaxTempForFiveDays.maxTemp - getMaxTempForFiveDays.minTemp) + 'px'}"></div>
-            <div>{{ dayWeather.main.temp }}°</div>
+    <div class="weatherOnFiveDays">
+        <h1 class="weatherOnFiveDaysTitle">{{ weatherOnFiveDays.city.name }}</h1>
+        <div class="weatherOnFiveDaysTable" v-if="weatherOnFiveDays">
+            <div class="eachDay" v-for="dayWeather in weatherOnFiveDays.list">
+                <div class="eachDayColumn" :style="{height: 5 + 95 * (dayWeather.main.temp - getMaxTempForFiveDays.minTemp) / (getMaxTempForFiveDays.maxTemp - getMaxTempForFiveDays.minTemp) + 'px'}"></div>
+                <div class="eachDayIndicator">{{ dayWeather.main.temp }}°</div>
+                <img :src="getIcon(dayWeather.weather[0]).imgSrc" :alt="getIcon(dayWeather.weather[0]).imgAlt">
+                <div class="eachDayIndicator">{{ dayWeather.clouds.all }}%</div>
+                <div class="eachDayIndicator">{{ dayWeather.pop }}%</div>
+                <div class="eachDayIndicator">{{ dayWeather.main.humidity }}%</div>
+                <div class="eachDayIndicator">{{ dayWeather.main.pressure }}мм</div>
+                <div class="eachDayIndicator">{{ dayWeather.wind.deg }}°</div>
+                <div class="eachDayIndicator">{{ (dayWeather.wind.speed).toFixed(0) }}м/с</div>
+                <div class="eachDayIndicator">{{ (dayWeather.wind.gust).toFixed(0) }}м/с</div>
+                <div class="eachDayIndicator">{{ (dayWeather.visibility / 1000).toFixed(1) }}км</div>
+            </div>
         </div>
     </div>
 </template>
 
 <style>
+.weatherOnFiveDaysTitle {
+    text-align: center;
+}
+
 .weatherOnFiveDaysTable {
     display: flex;
     overflow-x: auto;
 }
 
 .eachDay {
-    width: 50px;
     min-width: 50px;
     display: flex;
     flex-direction: column;
@@ -52,5 +74,9 @@ export default {
 .eachDayColumn {
     width: 100%;
     background-color: darkblue;
+}
+
+.eachDayIndicator {
+    white-space: nowrap;
 }
 </style>
