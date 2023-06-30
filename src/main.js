159cacffc0a152704891a62ee34d9c30e7a0aeff
Juris-Berkulis <krztfjrbhm1@jmail.com>
2023-06-30 12:49:59 +0300

    1. Create a "styles" folder for all scss-files, move these files into it and change the corresponding imports.

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
-import { createApp } from 'vue'
-//? import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+//? import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
 import { components } from './components/UI';
 import store from './store';
 
-import './assets/main.scss'
+import './assets/styles/main.scss';
 
-const app = createApp(App)
+const app = createApp(App);
 
 components.forEach(component => {
     app.component(component.name, component); //* - Регистрация с объектом настроек. Первый аргумент - название компонента; второй аргумент - сам компонент.
 });
 
-//? app.use(createPinia())
-app.use(store)
-app.use(router)
+//? app.use(createPinia());
+app.use(store);
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
